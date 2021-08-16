55a24d6f4d59b19571ae9ea570ab4f543e4bf294
github-action-benchmark <github@users.noreply.github.com>
2021-08-16 17:33:58 +0000

    add Soak Test Benchmarks (custombenchmark) benchmark result for a12cb5994c63c4d3193cab69f19da6d02d61604f

---
+++ benchmarks/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1629135238806,
+  "repoUrl": "https://github.com/NathanielRN/aws-otel-python-soaking-tests-demo",
+  "entries": {
+    "Soak Test Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "nathanielruiz98@gmail.com",
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN"
+          },
+          "committer": {
+            "email": "nathanielruiz98@gmail.com",
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN"
+          },
+          "distinct": true,
+          "id": "a12cb5994c63c4d3193cab69f19da6d02d61604f",
+          "message": "Add Soak Test Results to gh-pages webpage",
+          "timestamp": "2021-08-16T10:33:36-07:00",
+          "tree_id": "372f6d6928f6ca981a3c769b08643ff1b904ec62",
+          "url": "https://github.com/NathanielRN/aws-otel-python-soaking-tests-demo/commit/a12cb5994c63c4d3193cab69f19da6d02d61604f"
+        },
+        "date": 1629135237817,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test CPU Load AVG",
+            "value": 178.89772727272728,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Memory Used AVG",
+            "value": 155658240,
+            "unit": "Bytes"
+          }
+        ]
+      }
+    ]
+  }
+}
